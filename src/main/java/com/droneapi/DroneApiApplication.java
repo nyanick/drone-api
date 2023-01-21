96b9a5082ebd8cdc1f3c5b179f82e59d0cadd2d2
nyanick <yanicknde@gmail.com>
2023-01-22 00:53:22 +0100

    Initial project structure setup from intellij

---
+++ src/main/java/com/droneapi/DroneApiApplication.java
@@ -0,0 +1,13 @@
+package com.droneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneApiApplication.class, args);
+	}
+
+}
